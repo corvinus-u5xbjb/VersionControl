2e3787507d6123bdf8562f1315385265842ebbd9
Bálint Kiss <kissb2000@gmail.com>
2021-10-08 12:42:14 +0200

    Formázás

--- ExcelWeek04/ExcelWeek04/Form1.cs
+++ ExcelWeek04/ExcelWeek04/Form1.cs
@@ -100,6 +100,15 @@ namespace ExcelWeek04
             }
 
             object[,] values = new object[Flats.Count, headers.Length];
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
         }
         private string GetCell(int x, int y)
         {
