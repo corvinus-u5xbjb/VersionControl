5e606f87191d8422ffbae025e554770b6ff07f43
Bálint Kiss <kissb2000@gmail.com>
2021-10-08 11:12:28 +0200

    Projekt létrehozása

---
+++ ExcelGenerálás/ExcelGenerálás/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExcelGenerálás
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
