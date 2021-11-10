316209c43d80c602da11660f125de68609aef2f9
Bálint Kiss <kissb2000@gmail.com>
2021-11-10 09:48:12 +0100

    loading car image

---
+++ UserMaintenance/PresentFactory/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using PresentFactory.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentFactory.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
