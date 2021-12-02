9477d3f1d0e13ac16409253cbda41bd0bfbe54e2
Bálint Kiss <kissb2000@gmail.com>
2021-12-02 09:27:57 +0100

    Szimulációs ciklus

--- UserMaintenance/MicroSimulation/Entities/Gender.cs
+++ UserMaintenance/MicroSimulation/Entities/Gender.cs
@@ -11,12 +11,4 @@ namespace MicroSimulation.Entities
         Male = 1,
         Female = 2
     }
-
-    Gender ádámNeme = Gender.Male;
-    Gender évaNeme = (Gender)2;
-
-    if (ádámNeme == Gender.Male)
-	{
-
-	}
 }
