66a325f18457f6b781b4be430b6b6f52443bf970
Bálint Kiss <kissb2000@gmail.com>
2021-12-06 15:35:31 +0100

    Verseny felépítése

--- UserMaintenance/Evolúciós algoritmus/Form1.Designer.cs
+++ UserMaintenance/Evolúciós algoritmus/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace Evolúciós_algoritmus
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +43,22 @@ namespace Evolúciós_algoritmus
             this.label1.Text = "1. generáció";
             this.label1.UseWaitCursor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +70,7 @@ namespace Evolúciós_algoritmus
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
